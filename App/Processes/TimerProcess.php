0b69cb8cb3f07a1b09de2bec4234cbf02c420e18
lvlei <garenlyu@gmail.com>
2021-08-25 17:27:05 +0800

    update

--- App/Processes/TimerProcess.php
+++ App/Processes/TimerProcess.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Processes;
 
+use App\Tasks\TestTask;
 use App\Timers\OrderTimer;
 use EasySwoole\Component\Process\AbstractProcess;
 
@@ -10,10 +11,10 @@ class TimerProcess extends AbstractProcess
     {
         echo 'Timer 进程启动'.PHP_EOL;
         
-        //订单支付查询
-        OrderTimer::getInstance()->orderPayQuery();
-
-        //订单充值查询
-        OrderTimer::getInstance()->orderTopUpQuery();
+        // 每隔 10 秒执行一次
+        \EasySwoole\Component\Timer::getInstance()->loop(10 * 1000, function () {
+            //投递测试任务
+            \EasySwoole\EasySwoole\Task\TaskManager::getInstance()->async(new TestTask(['user' => 'custom']));
+        });
     }
 }
