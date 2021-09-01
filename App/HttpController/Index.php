3f3bf7649b6611d8c3666f982e87cde714c8f313
lvlei <garenlyu@gmail.com>
2021-09-01 15:03:38 +0800

    update

---
+++ App/HttpController/Index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* api权限和中间件
+* @author   garenlyu
+*/ 
+namespace App\HttpController;
+
+use App\HttpMiddleware\Base;
+use EasySwoole\Template\Render;
+
+class Index extends Base
+{
+    public function index()
+    {
+        $this->display['content'] = '你好';
+        return $this->response()->write(Render::getInstance()->render('example.index', $this->display));
+    }
+}
